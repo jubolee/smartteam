8e265711ec1add69a196176a3de1a0d30f6ea8e0
Reily Siegel <mail@reilysiegel.com>
2023-04-20 18:27:16 -0400

    Simplify scripts.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 echo "Bluetooth connection opened. Kill With './end_bluetooth_connection.sh or with Ctrl+C'" &
-sudo rfcomm connect hci0 00:14:03:05:59:51
+sudo rfcomm connect hci0 00:14:03:05:59:51 &
+sleep 5; sudo python3 main.py
+
